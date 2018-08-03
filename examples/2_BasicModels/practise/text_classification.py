84b4edba91867ee595cdd266e26059a370d0d519
ottermegazord <idaly.ali@live.fr>
2018-08-03 15:50:54 +0800

    finishes text_classification

--- examples/2_BasicModels/practise/text_classification.py
+++ examples/2_BasicModels/practise/text_classification.py
@@ -1,6 +1,7 @@
 
 import tensorflow as tf
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -95,3 +96,42 @@ history = model.fit(partial_x_train,
 results = model.evaluate(test_data, test_labels)
 
 print(results)
+
+"""Visualizing a graph of accuracy and loss over time"""
+
+# returns a history object contaiinign everything that happened during training
+
+history_dict = history.history
+history_dict.keys()
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()   # clear figure
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
